2e9bb2fce228e63325fe08f7938ed41da66f841b
emanuelhardwell <emanuelnoe16@gmail.com>
2021-02-15 20:52:46 -0600

    crud of notes and users created successfully

--- backend/src/controllers/notes.controllers.js
+++ backend/src/controllers/notes.controllers.js
@@ -1,11 +1,40 @@
 /*  */
 const notesCtrl = {};
+const Note = require("../models/Note");
 
-notesCtrl.getNotes = (req, res) => res.send("Hola USERS");
-notesCtrl.createNote = (req, res) => res.send("Hola USERS");
+notesCtrl.getNotes = async (req, res) => {
+  const notes = await Note.find();
+  res.json(notes);
+  /*  res.json({ mesagge: "All notes" }); */
+};
+notesCtrl.createNote = async (req, res) => {
+  const { title, content, date, author } = req.body;
+  const newNote = new Note({
+    title,
+    content,
+    date,
+    author,
+  });
+  /* console.log(newNote); */
+  await newNote.save();
 
-notesCtrl.getNote = (req, res) => res.send("Hola USERS");
-notesCtrl.updateNote = (req, res) => res.send("Hola USERS");
-notesCtrl.deleteNote = (req, res) => res.send("Hola USERS");
+  res.json({ message: "Note SAVED Successfully" });
+};
+
+notesCtrl.getNote = async (req, res) => {
+  const note = await Note.findById(req.params.id);
+  res.json(note);
+};
+
+notesCtrl.updateNote = async (req, res) => {
+  const { title, content, author } = req.body;
+  await Note.findByIdAndUpdate(req.params.id, { title, content, author });
+  res.json({ message: "Note UPDATE Successfully" });
+};
+
+notesCtrl.deleteNote = async (req, res) => {
+  await Note.findByIdAndDelete(req.params.id);
+  res.json({ message: "Note DELETE Successfully" });
+};
 
 module.exports = notesCtrl;
--- backend/src/controllers/users.controllers.js
+++ backend/src/controllers/users.controllers.js
@@ -1,8 +1,25 @@
 /*  */
 const usersCtrl = {};
+const User = require("../models/User");
 
-usersCtrl.getUsers = (req, res) => res.send("Hola USERS");
-usersCtrl.createUser = (req, res) => res.send("Hola USERS");
-usersCtrl.deleteUser = (req, res) => res.send("Hola USERS");
+usersCtrl.getUsers = async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+};
+
+usersCtrl.createUser = async (req, res) => {
+  const { username } = req.body;
+  const newUser = new User({
+    username,
+  });
+
+  await newUser.save();
+  res.json({ message: "User SAVED Successfully" });
+};
+
+usersCtrl.deleteUser = async (req, res) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.json({ message: "User DELETE Successfully" });
+};
 
 module.exports = usersCtrl;
