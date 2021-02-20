1e3d63327e65e848f715a9660fbfe00e4badbac1
emanuelhardwell <emanuelnoe16@gmail.com>
2021-02-19 22:17:53 -0600

    it was created Add and Delete users

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -3,17 +3,53 @@ import axios from "axios";
 
 export const CreateUser = () => {
   /*   componentDidMount */
-  const [user, setUser] = useState([]);
+
+  const [user, setUser] = useState({
+    users: [],
+  });
+
+  const [input, setInput] = useState({
+    username: "",
+  });
+
+  const { users } = user;
+  const { username } = input;
 
   const getData = async () => {
     const { data } = await axios.get("http://localhost:5000/api/users");
-    /* console.log(data); */
-    setUser(data);
+    console.log(data);
+    setUser({
+      ...user,
+      users: data,
+    });
   };
 
   useEffect(() => {
     getData();
-  }, [user]);
+  }, []);
+
+  const handleChange = ({ target }) => {
+    setInput({
+      ...input,
+      [target.name]: target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:5000/api/users", {
+      username: username,
+    });
+    setInput({
+      username: "",
+    });
+    getData();
+  };
+
+  const handleDelete = async (id) => {
+    await axios.delete(`http://localhost:5000/api/users/${id}`);
+    getData();
+  };
 
   return (
     /*  */
@@ -24,13 +60,16 @@ export const CreateUser = () => {
             <h3>Create new user</h3>
           </div>
           <div className="card-body">
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="form-group mb-3">
                 <input
                   type="text"
                   placeholder="name"
                   autoComplete="off"
                   className="form-control"
+                  name="username"
+                  value={username}
+                  onChange={handleChange}
                 />
               </div>
               <div className="form-group">
@@ -42,9 +81,15 @@ export const CreateUser = () => {
       </div>
       <div className="col-md-8">
         <ul className="list-group">
-          <li className="list-group-item text-center bg-primary">Lis of users</li>
-          {user.map((u) => (
-            <li className="list-group-item list-group-item-action" key={u._id}>
+          <li className="list-group-item text-center bg-primary">
+            Lis of users
+          </li>
+          {users.map((u) => (
+            <li
+              className="list-group-item list-group-item-action"
+              key={u._id}
+              onDoubleClick={() => handleDelete(u._id)}
+            >
               {u.username}
             </li>
           ))}
